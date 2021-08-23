01cff7ed7673bb66b66430bd39c402b07837ac48
= <walker.richard.james@gmail.com>
2021-08-23 11:23:44 +0100

    Added a function to add two numbers

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def my_function(a, b):
+    return a + b
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Richard')
     print_hi('Dude')
+    print(my_function(2, 4))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
