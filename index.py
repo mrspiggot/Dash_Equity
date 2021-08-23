c011070b6d338667db2b1fa7d41a2ec351d9df44
= <walker.richard.james@gmail.com>
2021-08-23 15:04:21 +0100

    Index Landing page added Navbar

---
+++ index.py
@@ -0,0 +1,70 @@
+from app import app
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
+
+input_analysis_dropdown = dbc.DropdownMenu(
+    children=[
+        dbc.DropdownMenuItem("OxCGRT", href='/oxford'),
+        dbc.DropdownMenuItem("Twitter Sentiment", href='/twitter'),
+    ],
+    nav=True,
+    in_navbar=True,
+    label="Equity Analysis",
+)
+
+navbar = dbc.Navbar(
+    dbc.Container(
+        [
+            html.A(
+                # Use row and col to control vertical alignment of logo / brand
+                dbc.Row(
+                    [
+                        dbc.Col(html.Img(src='assets/lucidate.png', height="90px")),
+                        dbc.Col(dbc.NavbarBrand("Lucidate Sample Dash Finance App", className="ml-2", href='/home')),
+                    ],
+                    align="center",
+                    no_gutters=True,
+                ),
+                href="/",
+            ),
+            dbc.Nav(
+                [dbc.NavItem(dbc.NavLink("Charts", href="/charts", disabled=False)),
+                 dbc.NavItem(dbc.NavLink("Heatmaps", href="/heatmaps")),
+                 dbc.NavItem(dbc.NavLink("Benchmarks", href="/benchmarks")),
+                 dbc.NavItem(dbc.NavLink("Composition", href="/composition")),
+                 dbc.NavItem(dbc.NavLink("Rebalancing", href="/rebalancing")),
+                 dbc.NavItem(dbc.NavLink("Sankey", href="/sankey")),
+                 dbc.NavItem(dbc.NavLink("VaR", href="/Value_At_Risk")),
+                 input_analysis_dropdown,
+
+                 ], className="ml-auto", navbar=True
+            ),
+        ]
+    ),
+    color="dark",
+    dark=True,
+    className="mb-5",
+)
+
+CONTENT_STYLE = {
+    "margin-left": "2rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+def serve_layout():
+    return html.Div([
+    dcc.Location(id='page-url'),
+    html.Div([
+        navbar,
+    ]),
+    html.Div(id='page-content', children=[]),
+],
+style=CONTENT_STYLE)
+
+app.layout = serve_layout
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8027)
