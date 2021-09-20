4e50e605a6ec8e19609646a2df0dbff4b268ea0c
mrspiggot <walker.richard.james@gmail.com>
2021-09-20 08:17:35 +0100

    Refactored Stocks Model for chart view

--- apps/charts.py
+++ apps/charts.py
@@ -3,57 +3,11 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
-import yahoo_fin.stock_info as si
 import plotly.graph_objects as go
-from datetime import date, timedelta
+from datetime import timedelta
+from utils.model_stocks import Model
 
-class Stocks():
-    def __init__(self):
-        self.tickers = ['gs', 'jpm', 'msft', 'aapl']
-        self.names = ['Goldman Sachs', 'JP Morgan', 'Microsoft', 'Apple']
-        self.ticker_dict = dict(zip(self.tickers, self.names))
-        self.today = date.today()
-        self.dropdown_options = self.populate_dropdown()
-        self.date_dict = {1: 30, 2: 60, 3: 90, 4: 180, 5: 360, 6:720}
-
-    def get_quotes(self, start_date):
-        ''' This function takes in a start date in order to get a range of stock quote data.
-        It returns a dictionary, the key of the dictionary is the ticker and the
-        value of each element in the dictionary is a dataframe that contains the OHLCV data
-        for that ticker from the start date through to today'''
-        quote_dict = {}
-        for ticker in self.tickers:
-            df = si.get_data(ticker, start_date=start_date, end_date=self.today)
-            quote_dict[ticker] = df
-
-        return quote_dict
-
-    def get_daily_returns(self, quotes):
-        '''This function takes as an input a dictionary of quotes; the key of each dictionary is a stock ticker,
-        the value of each element is a dataframe that contains the OHLCV data for that stock over a period of
-        time. The function dictionary with the stock ticker as its key and the OHLCV data transposed into a series of
-        the cumulative daily returns for that stock'''
-
-        returns_dict = {}
-        for ticker, quote_df in quotes.items():
-            daily_returns = quote_df['adjclose'].pct_change()
-            cum_daily_returns = (daily_returns+1).cumprod()
-            returns_dict[ticker] = cum_daily_returns
-
-        return returns_dict
-
-    def populate_dropdown(self):
-        ''' Dash requires a list of dictionaries for labels and values to configure a dropdown list. As we
-        have the names and ticker symbols as part of the attricutes of the Stocks class we can use this method
-        to build this list of dictionaries'''
-        options_list = []
-        for ticker, name in self.ticker_dict.items():
-            d = {'label': name, 'value': ticker}
-            options_list.append(d)
-
-        return options_list
-
-stocks = Stocks()
+stocks = Model()
 layout = html.Div([
     dbc.Row([
         dbc.Col(dcc.Dropdown(id='ohlc-dropdown',
@@ -88,6 +42,7 @@ layout = html.Div([
               [Input('ohlc-dropdown', 'value'),
                Input('date-slider', 'value')])
 def single_stock_ohlc_chart(ticker, date_range):
+
     quotes = stocks.get_quotes(stocks.today-timedelta(days=stocks.date_dict[date_range]))
     df = quotes[ticker].iloc[date_range:]
 
@@ -104,6 +59,7 @@ def single_stock_ohlc_chart(ticker, date_range):
                Input('date-slider', 'value'),
                Input('type-slider', 'value')])
 def multi_stock_returns_chart(ticker, date_range, price_or_return):
+
     quotes = stocks.get_quotes(stocks.today-timedelta(days=stocks.date_dict[date_range]))
     returns = stocks.get_daily_returns(quotes)
 
@@ -114,25 +70,16 @@ def multi_stock_returns_chart(ticker, date_range, price_or_return):
         y_axis_text = 'Stock Price ($)'
         for stock_ticker, prices in quotes.items():
             if stock_ticker in ticker:
-                fig.add_trace(go.Scatter(x=prices.index.values,
-                                         y=prices['adjclose'],
-                                         name=stocks.ticker_dict[stock_ticker]))
+                fig.add_trace(go.Scatter(x=prices.index.values, y=prices['adjclose'], name=stocks.ticker_dict[stock_ticker]))
     else:
         title = 'Multi Stock Returns'
         y_axis_text = 'Normalised returns'
         for stock_ticker, daily_returns in returns.items():
             if stock_ticker in ticker:
-                fig.add_trace(go.Scatter(x=daily_returns.index.values,
-                                     y=daily_returns.values,
-                                     name=stocks.ticker_dict[stock_ticker]))
+                fig.add_trace(go.Scatter(x=daily_returns.index.values, y=daily_returns.values, name=stocks.ticker_dict[stock_ticker]))
 
-    fig.update_layout(font=dict(color='#58C'), height=850,
-                      title=title,
-                      xaxis_title='Date',
-                      yaxis_title=y_axis_text,
-                      plot_bgcolor='#222',
-                      paper_bgcolor='#222'
-                      )
+    fig.update_layout(font=dict(color='#58C'), height=850, title=title, xaxis_title='Date', yaxis_title=y_axis_text,
+                      plot_bgcolor='#222', paper_bgcolor='#222')
 
     return fig
 
---
+++ utils/model_stocks.py
@@ -0,0 +1,39 @@
+import yahoo_fin.stock_info as si
+from datetime import date
+
+class Model():
+    def __init__(self):
+        self.tickers = ['gs', 'jpm', 'msft', 'aapl']
+        self.names = ['Goldman Sachs', 'JP Morgan', 'Microsoft', 'Apple']
+        self.ticker_dict = dict(zip(self.tickers, self.names))
+        self.today = date.today()
+        self.dropdown_options = self.populate_dropdown()
+        self.date_dict = {1: 30, 2: 60, 3: 90, 4: 180, 5: 360, 6:720}
+    def get_quotes(self, start_date):
+        ''' This function takes in a start date in order to get a range of stock quote data. It returns a dictionary,
+        the key of the dictionary is the ticker and the value of each element in the dictionary is a dataframe that
+        contains the OHLCV data for that ticker from the start date through to today'''
+        quote_dict = {}
+        for ticker in self.tickers:
+            df = si.get_data(ticker, start_date=start_date, end_date=self.today)
+            quote_dict[ticker] = df
+        return quote_dict
+    def get_daily_returns(self, quotes):
+        '''This function takes as an input a dictionary of quotes; the key of each dictionary is a stock ticker,
+        the value of each element is a dataframe that contains the OHLCV data for that stock over a period of
+        time. The function dictionary with the stock ticker as its key and the OHLCV data transposed into a series of
+        the cumulative daily returns for that stock'''
+        returns_dict = {}
+        for ticker, quote_df in quotes.items():
+            daily_returns = quote_df['adjclose'].pct_change()
+            cum_daily_returns = (daily_returns+1).cumprod()
+            returns_dict[ticker] = cum_daily_returns
+        return returns_dict
+    def populate_dropdown(self):
+        ''' Dash requires a list of dictionaries for labels and values to configure a dropdown list. We have the names
+        and tickers as attributes of the Model class we can use this method to build this list of dictionaries'''
+        options_list = []
+        for ticker, name in self.ticker_dict.items():
+            d = {'label': name, 'value': ticker}
+            options_list.append(d)
+        return options_list
