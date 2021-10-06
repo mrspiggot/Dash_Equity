540b869b0ab4c21f5121df6861e5ffc06ba68174
mrspiggot <walker.richard.james@gmail.com>
2021-10-06 16:05:00 +0100

    Added change card in ticker.py

--- apps/ticker.py
+++ apps/ticker.py
@@ -7,6 +7,7 @@ from utils.sp500 import StockInfo, SP500
 from utils.generate_portfolio_data import PortfolioDataGenerator
 import dash_daq as daq
 import yahoo_fin.stock_info as si
+import plotly.graph_objects as go
 
 port = PortfolioDataGenerator('assets/Sample Portfolio.xlsx')
 sp500 = SP500()
@@ -22,11 +23,109 @@ layout = html.Div([
                                   labelPosition='top'))
     ]),
     dbc.Row([
-        dbc.Card(id='card-ticker', children=[]
+        dbc.CardDeck(id='card-ticker', children=[]
 
         )
     ])
 ])
+def fill_simple_quote(name, ticker, ccy, live, delta):
+    if delta < 0:
+        colour = "#EE9900"
+        sign = ""
+    else:
+        colour = "#00AAAA"
+        sign = "+"
+
+    return dbc.Card([dbc.CardHeader(html.H3(name, style={'white-space': 'normal'})),
+                dbc.CardBody([html.H4(ticker)]),
+                dbc.CardBody([dbc.Row([html.H6(ccy), html.H1("{:.2f}".format(live))]),
+                dbc.Row([html.H6(ccy, style={'color': colour}), html.H1(sign, style={'color': colour}),
+                html.H1("{:.2f}".format(delta), style={'color': colour})])])], style={'width': "18rem"})
+def generate_indicator(percent_change):
+    f = go.Indicator(mode="delta", value=0, number={'prefix': "$"},
+                                 delta={'position': 'top',
+                                        'reference': -percent_change,
+                                        'valueformat': '.2%',
+                                        "font":{"size":16}})
+    fig = go.Figure(f)
+    fig.layout.height=100
+    fig.layout.width=100
+    fig.layout.plot_bgcolor='#313131'
+    fig.layout.paper_bgcolor='#313131'
+
+    return fig
+def fill_change_bar(quote, live, t_label):
+    h = quote['high']
+    l = quote['low']
+    o = quote ['open']
+    if o > h:
+        h=o
+    if o < l:
+        l=o
+    if live > h:
+        h=live
+    if live < l:
+        l=live
+    hl_spread = h - l
+    now_low_spread = live - l
+    open_low_spread = o - l
+    val = int(100*now_low_spread/hl_spread)
+    if live > o:
+        start = 100*open_low_spread/hl_spread
+        colour_dict_up = {"ranges":{"white":[0,start], "cyan":[start,val-5], "blue":[val-5,val+5]}}
+        bar = daq.GraduatedBar(
+            vertical=True,
+            color=colour_dict_up,
+            max=100,
+            value=val,
+            label=t_label,
+            labelPosition='top'
+        )
+    else:
+        end = 100 * open_low_spread / hl_spread
+        colour_dict_down = {"ranges":{"blue":[val-2, val+2],"orange": [val+2,o], "white":[0, val]}}
+        bar = daq.GraduatedBar(
+            vertical=True,
+            color=colour_dict_down,
+            max=100,
+            value=o,
+            label=t_label,
+            labelPosition='top'
+        )
+
+    return bar
+
+def fill_changes(quote, live):
+    open_today = quote['today']['open']
+    open_month = quote['month']['open']
+    open_year = quote['year']['open']
+
+    dcp = (live-open_today)/open_today
+    mcp = (live-open_month)/open_month
+    ycp = (live-open_year)/open_year
+
+    fig_day = generate_indicator(dcp)
+    fig_mth = generate_indicator(mcp)
+    fig_yr = generate_indicator(ycp)
+
+    day = fill_change_bar(quote['today'], live, "Day change")
+    month = fill_change_bar(quote['month'], live, "Month change")
+    year = fill_change_bar(quote['year'], live, "Year change")
+
+    dg = dcc.Graph(id='day-ind', figure=fig_day)
+    mg = dcc.Graph(id='mth-ind', figure=fig_mth)
+    yg = dcc.Graph(id='yr-ind', figure=fig_yr)
+
+    day_col = dbc.Col([dg, day])
+    month_col = dbc.Col([mg, month])
+    year_col = dbc.Col([yg, year])
+
+    change_card = dbc.Card([
+        dbc.CardHeader(html.H4("Day, Month, Year OHL-Live Prices")),
+        dbc.CardBody(dbc.Row([day_col, month_col, year_col], style={"white-space": "nowrap"}))],
+        style={'width': "36rem"}
+    )
+    return change_card
 
 @app.callback(Output('card-ticker', 'children'),
               [Input('dd-ticker-ticker', 'value'),
@@ -36,13 +135,11 @@ def load_sec_info(ticker, scope):
     stock = sp500.stock_dict[ticker]
     open = stock.quotes_dict['today']['open']
     delta = live - open
-    if delta < 0:
-        colour = "#EE9900"
-    else:
-        colour = "#00AAAA"
-    card_list = [dbc.CardHeader(html.H3(stock.name)),
-                dbc.CardBody([html.H4(stock.ticker)]),
-                  dbc.CardBody([dbc.Row([html.H6("$"), html.H2("{:.2f}".format(live))]),
-                               dbc.Row([html.H6("$"), html.H2("{:.2f}".format(delta), style={'color': colour})])])]
 
+    card_list = []
+    simple_quote = fill_simple_quote(stock.name, stock.ticker, "$", live, delta)
+
+    card_list.append(simple_quote)
+    changes = fill_changes(stock.quotes_dict, live)
+    card_list.append(changes)
     return card_list
